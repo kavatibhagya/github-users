5055243ca8da91a5b8ab2903c2424d72fb119b74
kavatibhagya <bhagyalakshmi.kavati@gmail.com>
2020-03-20 09:01:15 -0700

    showing users list

--- main.js
+++ main.js
@@ -4,11 +4,15 @@ function gitFetch(){
         
         return response.json()
     })
-    .then(function (data){
+    .then(function (data)
+    {
        // console.log(data);
-       for(let i = 0; i < data.length; i++ ){
-        console.log(data[i].login);
-       }
+            for(let i = 0; i < data.length; i++ ){
+          
+             const output = document.createElement('div');
+             output.innerText = data[i].login;
+             document.body.appendChild(output);
+            }
     })
 
 
