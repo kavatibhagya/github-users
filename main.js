4b4fe88af6cb27c8b9df9a34b1411204ddcf7ae6
kavatibhagya <bhagyalakshmi.kavati@gmail.com>
2020-03-21 20:39:25 -0700

    added my git data to the git users data

--- main.js
+++ main.js
@@ -1,3 +1,44 @@
+function myGitFetch() {
+    fetch("https://api.github.com/users/kavatibhagya")
+    .then(response => response.json())
+    .then(data => 
+        {
+            const myData = document.getElementById('myData');
+            const myname = document.createElement('div');
+            myname.innerText = data.name;
+            myname.classList.add('name');
+            myData.appendChild(myname);
+
+            const loginId = document.createElement('div');
+            loginId.innerText = data.login;
+            myData.appendChild(loginId);
+            loginId.classList.add('loginStyle');
+        
+
+        
+
+            const myImage = document.createElement('img');
+            myImage.setAttribute("src", data['avatar_url']);
+            myImage.setAttribute("height","300");
+            myImage.setAttribute("width" , "300");
+            myImage.classList.add('image');
+            myData.appendChild(myImage);
+
+            const myLocation = document.createElement('div');
+            myLocation.innerText = data.location;
+            myData.appendChild(myLocation);
+
+
+        })
+
+    
+
+}
+
+
+myGitFetch();
+
+
 function gitFetch(){
     fetch("https://api.github.com/users")
     .then(function(response) { 
@@ -11,10 +52,11 @@ function gitFetch(){
             for(let i = 0; i < data.length; i++ ){
           
              const output = document.createElement('div');
-             
              output.classList.add('container');
+
              const name = document.createElement('div');
               name.innerText = data[i].login;
+              name.classList.add('name');
               output.appendChild(name);
              
              const image = document.createElement('img');
