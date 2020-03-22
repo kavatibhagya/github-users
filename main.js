21d3a0ec112059bbee2d1cb6a84449bcb0beb141
kavatibhagya <bhagyalakshmi.kavati@gmail.com>
2020-03-21 19:41:03 -0700

    added git users data with images

--- main.js
+++ main.js
@@ -7,11 +7,24 @@ function gitFetch(){
     .then(function (data)
     {
        // console.log(data);
+            const result = document.getElementById('result');
             for(let i = 0; i < data.length; i++ ){
           
              const output = document.createElement('div');
-             output.innerText = data[i].login;
-             document.body.appendChild(output);
+             
+             output.classList.add('container');
+             const name = document.createElement('div');
+              name.innerText = data[i].login;
+              output.appendChild(name);
+             
+             const image = document.createElement('img');
+             image.setAttribute("src" , data[i]['avatar_url']);
+             image.setAttribute("height","300");
+             image.setAttribute("width" , "300");
+             image.classList.add('image');
+
+             output.appendChild(image);
+             result.appendChild(output);
             }
     })
 
